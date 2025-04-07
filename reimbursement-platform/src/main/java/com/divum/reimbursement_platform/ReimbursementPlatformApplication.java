b03638c30e164b17bc4e25338f1614c3d47998cc
Divakar A <adivakau@amazon.com>
2025-04-07 16:55:39 +0530

    Added Spring Intial package

---
+++ reimbursement-platform/src/main/java/com/divum/reimbursement_platform/ReimbursementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.divum.reimbursement_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReimbursementPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReimbursementPlatformApplication.class, args);
+	}
+
+}
